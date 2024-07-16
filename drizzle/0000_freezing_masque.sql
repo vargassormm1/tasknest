fda229ed65529e77f2669acb866e6376c4352a67
Ralph Vargas <ralphvargassorm@gmail.com>
2024-07-16 19:26:21 -0400

    signin and signup now redirects to new-user page

--- drizzle/0000_freezing_masque.sql
+++ drizzle/0000_freezing_masque.sql
@@ -1,12 +1,22 @@
 CREATE TABLE IF NOT EXISTS "tasknest_groups" (
-	"id" integer PRIMARY KEY NOT NULL,
+	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
 	"emoji" text,
 	"userId" integer
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "tasknest_todos" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"text" text NOT NULL,
+	"done" boolean DEFAULT false NOT NULL,
+	"dueDate" timestamp,
+	"created_at" timestamp DEFAULT now(),
+	"userId" integer,
+	"groupId" integer NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "tasknest_users" (
-	"id" integer PRIMARY KEY NOT NULL,
+	"id" serial PRIMARY KEY NOT NULL,
 	"clerkId" text NOT NULL,
 	"fname" text NOT NULL,
 	"lname" text NOT NULL,
@@ -14,11 +24,6 @@ CREATE TABLE IF NOT EXISTS "tasknest_users" (
 	"imageUrl" text NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "tasknest_todo" RENAME TO "tasknest_todos";--> statement-breakpoint
-ALTER TABLE "tasknest_todos" ADD COLUMN "dueDate" timestamp;--> statement-breakpoint
-ALTER TABLE "tasknest_todos" ADD COLUMN "created_at" timestamp DEFAULT now();--> statement-breakpoint
-ALTER TABLE "tasknest_todos" ADD COLUMN "userId" integer;--> statement-breakpoint
-ALTER TABLE "tasknest_todos" ADD COLUMN "groupId" integer NOT NULL;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "tasknest_groups" ADD CONSTRAINT "tasknest_groups_userId_tasknest_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."tasknest_users"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
---
+++ src/app/new-user/page.tsx
@@ -0,0 +1,51 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import db from "@/db/drizzle";
+import { users } from "@/db/schema";
+import { eq, and } from "drizzle-orm";
+
+const createNewUser = async () => {
+  try {
+    const user = await currentUser();
+
+    if (!user) {
+      redirect("/sign-in");
+    }
+
+    const userEmail = user.emailAddresses[0]?.emailAddress;
+
+    const userExists = await db
+      .select({
+        clerkId: users.clerkId,
+        email: users.email,
+      })
+      .from(users)
+      .where(
+        and(
+          eq(users.clerkId, user.id),
+          eq(users.email, user?.emailAddresses[0].emailAddress)
+        )
+      );
+
+    if (!userExists.length) {
+      await db.insert(users).values({
+        clerkId: String(user.id),
+        fname: String(user.firstName),
+        lname: String(user.lastName),
+        email: String(userEmail),
+        imageUrl: String(user.imageUrl),
+      });
+    }
+
+    redirect("/todo");
+  } catch (error) {
+    console.error("Error creating new user:", error);
+  }
+};
+
+const NewUser = async () => {
+  await createNewUser();
+  return <div>...loading</div>;
+};
+
+export default NewUser;
